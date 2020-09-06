f63b94425e3d237c3383663210a275b3444abce2
Marcin Kapela <marcin.kapela.programista@gmail.com>
2020-09-06 11:37:11 +0200

    migrate db, create routes to postsController

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+        <h1>Welcome to Laravel</h1>
+        <p>Just learning the Laravel framework</p>
+        <p>
+                <a href="/login" role="button" class="btn btn-primary btn-lg">Login</a>
+                <a href="/register" role="button" class="btn btn-success btn-lg">Register</a>
+        </p>
+</div>
+@endsection
+        
+
